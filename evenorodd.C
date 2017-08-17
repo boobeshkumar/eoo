e3ecb7b7468015ebf28b7ec6324fd9597ed83928
boobeshkumar <30952711+boobeshkumar@users.noreply.github.com>
2017-08-17 16:45:40 +0530

    even or odd
    Not an even number means an odd number

---
+++ evenorodd.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int b;
+printf("\nEnter a number");
+scanf("%d,&b);
+if(b%2==0)
+printf("\nThe number is an even");
+else
+printf("\nThe number is not an even");
+getch();
+}
